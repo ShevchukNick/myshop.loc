00aea05e0d5a6b10066bd0b9eb6874400d0e02fb
Nick <shevchukniki@gmail.com>
2023-05-03 23:13:47 +0300

    create class ErrorHandler

--- public/index.php
+++ public/index.php
@@ -7,5 +7,6 @@ if (PHP_MAJOR_VERSION < 8) {
 
 require_once dirname(__DIR__) . '/config/init.php';
 
-//new \wfm\App();
+new \wfm\App();
+//throw new Exception('Возникла ошибка',404);
 //var_dump(\wfm\App::$app->getProperties());
