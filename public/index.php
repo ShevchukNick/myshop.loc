e9bb72b1a618618218418ce6429e1d4ef3bfb122
Nick <shevchukniki@gmail.com>
2023-05-04 16:33:27 +0300

    create class Router

--- public/index.php
+++ public/index.php
@@ -6,7 +6,9 @@ if (PHP_MAJOR_VERSION < 8) {
 
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
-//throw new Exception('Возникла ошибка',404);
-//var_dump(\wfm\App::$app->getProperties());
+
+debug(\wfm\Router::getRoutes());
