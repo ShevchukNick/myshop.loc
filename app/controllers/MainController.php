b9f0c31fba161100f67ff3a0bbbe6b10c3a210e7
Nick <shevchukniki@gmail.com>
2023-05-10 14:06:08 +0300

    add slider

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,19 +2,17 @@
 
 namespace app\controllers;
 
-
 use app\models\Main;
-use wfm\Controller;
+use RedBeanPHP\R;
 
 /** @property Main $model */
-class MainController extends Controller
+class MainController extends AppController
 {
 
     public function indexAction()
     {
-        $names = $this->model->get_names();
-        $this->setMeta('Главная страница', 'Description...', 'keywords...');
-        $this->set(compact('names'));
+        $slides = R::findAll('slider');
+        $this->set(compact('slides'));
     }
 
 }
