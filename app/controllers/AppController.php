fbcaf7c231481f92f0703f747163caa2e7633d2c
Nick <shevchukniki@gmail.com>
2023-05-11 16:25:37 +0300

    create method getLanguage

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -15,6 +15,7 @@ class AppController extends Controller
         parent::__construct($route);
         new AppModel();
         App::$app->setProperty('languages',Language::getLanguages());
-        debug(App::$app->getProperty('languages'));
+        App::$app->setProperty('language',Language::getLanguage(App::$app->getProperty('languages')));
+
     }
 }
--- app/widgets/language/Language.php
+++ app/widgets/language/Language.php
@@ -3,6 +3,7 @@
 namespace app\widgets\language;
 
 use RedBeanPHP\R;
+use wfm\App;
 
 class Language
 {
@@ -12,12 +13,14 @@ class Language
 
     public function __construct()
     {
-        $this->tpl=DIR . 'lang_tpl.php';
+        $this->tpl= __DIR__ . '/lang_tpl.php';
         $this->run();
+        echo $this->getHtml();
     }
     protected function run()
     {
-
+        $this->languages=App::$app->getProperty('languages');
+        $this->language=App::$app->getProperty('language');
     }
     public static function getLanguages():array
     {
@@ -25,6 +28,24 @@ class Language
     }
     public static function getLanguage($languages)
     {
+        $lang= App::$app->getProperty('lang');
+        if ($lang && array_key_exists($lang,$languages)) {
+           $key = $lang;
+        } elseif (!$lang) {
+            $key = key($languages);
+        } else {
+            $lang=h($lang);
+            throw new \Exception("not found languages {$lang}",404);
+        }
+        $lang_info = $languages[$key];
+        $lang_info['code']=$key;
+        return $lang_info;
+    }
 
+    protected function getHtml ():string
+    {
+        ob_start();
+        require_once $this->tpl;
+        return ob_get_clean();
     }
 }
