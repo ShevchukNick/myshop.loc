3daf5b7c5691a776de5b022caef09f849e76655a
Nick <shevchukniki@gmail.com>
2023-05-12 14:56:09 +0300

    create widget class Menu

--- app/languages/en.php
+++ app/languages/en.php
@@ -3,4 +3,9 @@ return [
     'tpl_search' => 'Search...',
     'tpl_login' => 'Authorization',
     'tpl_signup' => 'Registration',
+    'tpl_comp'=>'Computers',
+    'tpl_tablet'=>'Tablets',
+    'tpl_notebook'=>'Notebooks',
+    'tpl_phone'=>'Phones',
+    'tpl_camera'=>'Cameras',
 ];
--- app/languages/ru.php
+++ app/languages/ru.php
@@ -3,4 +3,9 @@ return [
     'tpl_search' => 'Поиск...',
     'tpl_login' => 'Авторизация',
     'tpl_signup' => 'Регистрация',
+    'tpl_comp'=>'Компьютеры',
+    'tpl_tablet'=>'Планшеты',
+    'tpl_notebook'=>'Ноутбуки',
+    'tpl_phone'=>'Телефоны',
+    'tpl_camera'=>'Камеры',
 ];
