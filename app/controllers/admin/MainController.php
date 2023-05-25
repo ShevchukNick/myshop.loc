bef763bfa088ae5d540909cef741ff670874c222
Nick <shevchukniki@gmail.com>
2023-05-25 16:18:55 +0300

    create Admin version

---
+++ app/controllers/admin/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers\admin;
+
+class MainController extends AppController
+{
+    public function indexAction()
+    {
+        $title = 'Главная страница';
+
+        $this->setMeta('Админка :: Главная страница');
+        $this->set(compact('title'));
+    }
+    
+}
